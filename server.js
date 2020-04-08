2abca4fc161bfaa8beb6c98402f5933451817b7c
Saul Feliz <646634@gmail.com>
2020-04-08 05:27:18 -0500

    server up and running; server.js and package.json setup

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("API Running"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
