5f5534edc26edc7401c265a7df83ae1b2a040843
Saul Feliz <646634@gmail.com>
2020-04-10 17:55:39 -0500

    added backend validation to user registration request body

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("User route");
-});
+router.post(
+  "/",
+  [
+    check("firstName", "First name is required").not().isEmpty(),
+    check("lastName", "Last name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password of 3 or more characters"
+    ).isLength({ min: 3 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User route");
+  }
+);
 
 module.exports = router;
