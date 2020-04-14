edb05146ad4170689063535a2fc3db9516e90662
Saul Feliz <646634@gmail.com>
2020-04-14 12:46:13 -0500

    able to register user and save the data to mongo collection with avatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+const User = require("../../models/User.js");
 
 // @route   POST api/users
 // @desc    Register user
@@ -16,12 +20,66 @@ router.post(
       "Please enter a password of 3 or more characters"
     ).isLength({ min: 3 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User route");
+
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      phoneNumber,
+      fullAddress,
+    } = req.body;
+
+    try {
+      // see if user exists already
+
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // get user gravatar
+
+      const avatar = gravatar.url(email, {
+        s: "200", // size
+        r: "pg", // pg rated pics
+        d: "mm", // default image if user has no gravatr
+      });
+
+      user = new User({
+        // create instance of user
+        firstName,
+        lastName,
+        email,
+        password,
+        avatar,
+        phoneNumber,
+        fullAddress,
+      });
+
+      // encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      // save the user
+      await user.save();
+
+      // return jwt to login user on register
+
+      res.send("User registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
