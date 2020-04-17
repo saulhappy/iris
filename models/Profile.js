06c6cf68ecf19418dcfd46b8f822b5470abd8265
Saul Feliz <646634@gmail.com>
2020-04-17 12:56:53 -0500

    user profile model created

---
+++ models/Profile.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  about: {
+    type: String,
+    required: true,
+  },
+  neighborhood: {
+    type: String,
+  },
+  openForRequests: {
+    type: Boolean,
+    default: false,
+  },
+  helpWith: {
+    type: [String],
+    required: true,
+  },
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
