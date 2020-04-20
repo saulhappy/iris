fa0b92bd382922bc722d712acd9652178b0301b8
Saul Feliz <646634@gmail.com>
2020-04-20 13:06:35 -0500

    GET all profiles routes created; successful response sent back from Mongo

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -93,4 +93,21 @@ router.post(
   }
 );
 
+// @route   GET api/profile/me
+// @desc    Get all profiles
+// @access  Public
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", [
+      "firstName",
+      "avatar",
+    ]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
