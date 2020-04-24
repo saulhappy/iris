d5efe0ecbd44095bf900ea46f740af8704184523
Saul Feliz <646634@gmail.com>
2020-04-24 13:39:56 -0500

    added landing page, navbar, adjusted app css styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 
 const App = () => (
   <Fragment>
-    <h1>App</h1>
+    <Navbar />
+    <Landing />
   </Fragment>
 );
 
