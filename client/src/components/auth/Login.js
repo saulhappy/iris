751f52cff6bf08bdbed2de25180a55b11cf010ef
Saul Feliz <646634@gmail.com>
2020-05-01 15:22:55 -0500

    added redirect to login component when user is authenticated

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,10 @@
 import React, { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { login } from "../../actions/auth";
 
-const Login = ({ login }) => {
+const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -19,6 +19,11 @@ const Login = ({ login }) => {
     e.preventDefault();
     login(email, password);
   };
+
+  // redirecting if user logged in:
+  if (isAuthenticated) {
+    return <Redirect to='/home' />;
+  }
 
   return (
     <Fragment>
@@ -60,6 +65,11 @@ const Login = ({ login }) => {
 
 Login.propTypes = {
   login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
 };
 
-export default connect(null, { login })(Login);
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { login })(Login);
