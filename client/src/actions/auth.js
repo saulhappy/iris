95c3664aa9c3050b47f83cb26a052b9f025cb77c
Saul Feliz <646634@gmail.com>
2020-04-30 22:18:00 -0500

    created auth actions

---
+++ client/src/actions/auth.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import { setAlert } from "./alert";
+
+// Register the user
+
+export const register = ({
+  firstName,
+  lastName,
+  email,
+  fullAddress,
+  password,
+}) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const body = JSON.stringify({
+    firstName,
+    lastName,
+    email,
+    fullAddress,
+    password,
+  });
+
+  try {
+    const res = await axios.post("/api/users", body, config);
+
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data,
+    });
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+    }
+    dispatch({
+      type: REGISTER_FAIL,
+    });
+  }
+};
