c3afb24afcae676be2e204c339f9217e6671806d
Saul Feliz <646634@gmail.com>
2020-04-30 23:36:17 -0500

    imported setAuthToken function to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,16 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
+
 //Redux stuff
 import { Provider } from "react-redux";
 import store from "./store";
 import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   // load user once when component mounts
