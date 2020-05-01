aa04dcbe9a3bf0e24b71f192bce05e0b1ffba0ba
Saul Feliz <646634@gmail.com>
2020-05-01 14:47:37 -0500

    added user login functionality to auth action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -68,3 +68,33 @@ export const register = ({
     });
   }
 };
+
+// Login the user
+
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post("/api/auth", body, config);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data,
+    });
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+    }
+    dispatch({
+      type: LOGIN_FAIL,
+    });
+  }
+};
