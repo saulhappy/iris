d29489596c6247af44a54f3d53e19ceee18eef88
Saul Feliz <646634@gmail.com>
2020-05-02 17:31:28 -0500

    updated Navbar font awesome icons; updated Landing to redirect home if user logged in

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
 const Home = (props) => {
   return <div>Welcome home.</div>;
 };
 
-Home.propTypes = {};
+// Home.propTypes = {};
 
 export default Home;
