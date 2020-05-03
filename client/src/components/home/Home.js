3bacb8e5d6418752aa0d2a1ad7ec3a2e00332532
Saul Feliz <646634@gmail.com>
2020-05-03 13:16:45 -0500

    added ternary logic to when to show spinner in home component

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,14 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
 import { getCurrentProfile } from "../../actions/profile";
 
-const Home = ({ getCurrentProfile, auth, profile }) => {
+const Home = ({ getCurrentProfile, auth, profile: { profile, loading } }) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
 
-  return <div>Welcome home.</div>;
+  // if there is no profile and jsx of component still loading, show the spinner
+  return loading && profile === null ? <Spinner /> : <Fragment>test</Fragment>;
 };
 
 Home.propTypes = {
