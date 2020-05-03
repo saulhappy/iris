003541b0e7021e35d61d0e04a655b0f8eef82d6e
Saul Feliz <646634@gmail.com>
2020-05-03 13:59:43 -0500

    added dynamic shell of what to present based on whether or not user has set up a profile

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, Fragment } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
@@ -18,14 +19,24 @@ const Home = ({
     <Spinner />
   ) : (
     <Fragment>
+      <h1 className='large text-primary'>
+        Welcome home, {user && user.firstName}
+      </h1>
+
+      {/* alternative style, unsure what to go with yet:  
       <h1 className='large text-primary'>Home</h1>
       <p className='lead'>
         <i className='fas fa-house-user'> Hello {user && user.firstName}</i>
-      </p>
+      </p> */}
       {profile !== null ? (
         <Fragment>has profile</Fragment>
       ) : (
-        <Fragment>has no profile</Fragment>
+        <Fragment>
+          <p>You have not set up a profile.</p>
+          <Link to='/create-profile' className='btn btn-primary my-1'>
+            Create Profile
+          </Link>
+        </Fragment>
       )}
     </Fragment>
   );
