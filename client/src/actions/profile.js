dfbaf8f343cd764749628af67d57d9770302bca5
Saul Feliz <646634@gmail.com>
2020-05-06 17:31:36 -0500

    added create and edit profile actions with error alerts

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -18,3 +18,41 @@ export const getCurrentProfile = () => async (dispatch) => {
     });
   }
 };
+
+// create or update profile
+export const createProfile = (formData, history, edit = false) => async (
+  dispatch
+) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const res = await axios.post("/api/profile", formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert(edit ? "Profile Updated!" : "Profile Created!"));
+
+    // if creating a profile, redirect
+    if (!edit) {
+      history.push("/home");
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
