2608a0bde91e4bc8e0f490ed3cc510ac32841a46
Saul Feliz <646634@gmail.com>
2020-05-08 12:02:49 -0500

    added getProfiles function

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,6 +3,7 @@ import { setAlert } from "./alert";
 
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
   CLEAR_PROFILE,
   ACCOUNT_DELETED,
@@ -14,6 +15,24 @@ export const getCurrentProfile = () => async (dispatch) => {
     const res = await axios.get("/api/profile/me");
     dispatch({
       type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// get all profiles
+export const getProfiles = () => async (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
+
+  try {
+    const res = await axios.get("/api/profile");
+    dispatch({
+      type: GET_PROFILES,
       payload: res.data,
     });
   } catch (err) {
