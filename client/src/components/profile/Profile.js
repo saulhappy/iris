901785b06582f3aae38337c5acc2d4db19b7c49f
Saul Feliz <646634@gmail.com>
2020-05-08 18:41:49 -0500

    added logic to show edit profile button only on own user's profile show page

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,7 +5,12 @@ import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { getProfileByID } from "../../actions/profile";
 
-const Profile = ({ getProfileByID, profile: { profile, loading }, match }) => {
+const Profile = ({
+  getProfileByID,
+  profile: { profile, loading },
+  auth,
+  match,
+}) => {
   useEffect(() => {
     getProfileByID(match.params.id);
   }, [getProfileByID]);
@@ -19,6 +24,13 @@ const Profile = ({ getProfileByID, profile: { profile, loading }, match }) => {
           <Link to='/profiles' className='btn btn-light'>
             Back to Profiles
           </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to='/edit-profile' className='btn btn-dark'>
+                Edit Profile
+              </Link>
+            )}
         </Fragment>
       )}
     </Fragment>
