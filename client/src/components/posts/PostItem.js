7a64517c15c3c3808ffe9820e0b1acc29a91a82a
Saul Feliz <646634@gmail.com>
2020-05-10 12:20:32 -0500

    UI tweeks

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -8,33 +8,36 @@ const PostItem = ({
   auth,
   post: { _id, firstName, lastName, text, avatar, user, likes, comments, date },
 }) => (
-  <div class='post bg-white p-1 my-1'>
+  <div className='post bg-white p-1 my-1'>
     <div>
       <a href='profile.html'>
-        <img class='round-img' src={avatar} alt='' />
+        <img className='round-img' src={avatar} alt='' />
         <h4>
           {firstName} {lastName}
         </h4>
       </a>
     </div>
     <div>
-      <p class='my-1'>{text}</p>
-      <p class='post-date'>
+      <p className='my-1'>{text}</p>
+      <p className='post-date'>
         Posted on <Moment format='YYYY-MM-DD'>{date}</Moment>
       </p>
-      <button type='button' class='btn btn-light'>
-        <i class='fas fa-thumbs-up'></i>
-        <span>{likes.length}</span>
+      <button type='button' className='btn btn-light'>
+        <i className='fas fa-thumbs-up'></i>{" "}
+        <span> {likes.length > 0 && <span>{likes.length}</span>}</span>
       </button>
-      <button type='button' class='btn btn-light'>
-        <i class='fas fa-thumbs-down'></i>
+      <button type='button' className='btn btn-light'>
+        <i className='fas fa-thumbs-down'></i>
       </button>
-      <a href='post.html' class='btn btn-primary'>
-        Discussion <span class='comment-count'>{comments.length}</span>
-      </a>
+
+      {comments.length > 0 && (
+        <Link to={`/post/${_id}`} className='btn btn-primary'>
+          Comments <span className='comment-count'>{comments.length}</span>
+        </Link>
+      )}
       {!auth.loading && user === auth.user._id && (
-        <button type='button' class='btn btn-danger'>
-          <i class='fas fa-times'></i>
+        <button type='button' className='btn btn-danger'>
+          <i className='fas fa-times'></i> Delete Post
         </button>
       )}
     </div>
