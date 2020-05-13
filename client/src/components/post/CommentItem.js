d26af9e3cc0255612092f863b704ad8a203a53ad
Saul Feliz <646634@gmail.com>
2020-05-13 15:22:22 -0500

    added deleteComment functionality

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -3,11 +3,13 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
+import { deleteComment } from "../../actions/post";
 
 const CommentItem = ({
   postID,
   comment: { _id, text, firstName, lastName, avatar, user, date },
   auth,
+  deleteComment,
 }) => (
   <div class='post bg-white p-1 my-1'>
     <div>
@@ -23,6 +25,15 @@ const CommentItem = ({
       <p class='post-date'>
         Posted on <Moment format='YYYY-MM-DD'>{date}</Moment>
       </p>
+      {!auth.loading && user === auth.user._id && (
+        <button
+          onClick={(e) => deleteComment(postID, _id)}
+          type='button'
+          className='btn btn-danger'
+        >
+          <i className='fas fa-times'></i>
+        </button>
+      )}
     </div>
   </div>
 );
@@ -31,10 +42,11 @@ CommentItem.propTypes = {
   postID: PropTypes.number.isRequired,
   comment: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
+  deleteComment: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, {})(CommentItem);
+export default connect(mapStateToProps, { deleteComment })(CommentItem);
