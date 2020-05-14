d6bd5413384eb88c728acb771b6e609a36db1a19
Saul Feliz <646634@gmail.com>
2020-05-14 13:27:03 -0500

    created NotFound component

---
+++ client/src/components/layout/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+export const NotFound = () => {
+  return (
+    <Fragment>
+      <h1 className='x-large text-primary'>
+        <i className='fas fa-exclamation-triangle'></i>Page Not Found
+      </h1>
+      <p className='large'> ¯\_(ツ)_/¯ nothing to see here...</p>
+    </Fragment>
+  );
+};
