95df3926ae175eb3e04509f7a0778e4ce97f2b89
Saul Feliz <646634@gmail.com>
2020-05-19 14:03:06 -0500

    updated about text

--- client/src/components/layout/About.js
+++ client/src/components/layout/About.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 export default function About() {
-  return <div>this will be the about page</div>;
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Welcome to Iris</h1>
+      <p className='lead'> We're a community of helpers</p>
+      <div>
+        <p>
+          According to ancient Greek Mythology as described in Hesiod's
+          Theogony, Iris is the daughter of Thaumas and the Oceanid Electra, and
+          the sister of the Harpies: Aello and Ocypete. During the Titan
+          Battles, Iris was the messenger of the Olympian Gods.
+        </p>
+        <br></br>
+        <p>
+          Though Iris was principally associated with communication and
+          messages, she was also believed to aid in the fulfillment of humans'
+          prayers, either by fulfilling them herself or by bringing them to the
+          attention of other deities. She is also said to travel on the rainbow
+          while carrying messages from the gods to mortals.
+        </p>
+        <br></br>
+        <p>
+          In light of the Covid-19 pandemic, when I first thought of this site,
+          I thought the world needed more of this. I believe the world could
+          always use more of this. You can be a messenger of good for someone
+          else in need.
+        </p>
+        <br></br>
+        <p>Saul Feliz</p>
+      </div>
+    </Fragment>
+  );
 }
