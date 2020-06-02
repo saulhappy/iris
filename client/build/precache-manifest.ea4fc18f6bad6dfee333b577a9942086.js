f98d780895c17f691cf50bf8eb3879ced4846eb9
Saul Feliz <646634@gmail.com>
2020-06-02 18:12:27 -0500

    product build

---
+++ client/build/precache-manifest.ea4fc18f6bad6dfee333b577a9942086.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2ebd9388deb6831dddc1ec88d4324e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3edba3758a68d59aea5",
+    "url": "/static/css/main.a44b08a0.chunk.css"
+  },
+  {
+    "revision": "cf7ce64b76bbc9012775",
+    "url": "/static/js/2.e53e1d7c.chunk.js"
+  },
+  {
+    "revision": "e293c143e177a32373554d6ca4c08bba",
+    "url": "/static/js/2.e53e1d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3edba3758a68d59aea5",
+    "url": "/static/js/main.37d9a869.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  },
+  {
+    "revision": "cc22aa259750c03126f942ec5c22ad2d",
+    "url": "/static/media/rainbowHand.cc22aa25.jpg"
+  },
+  {
+    "revision": "72dfb9c8aee6d0695fe28f57d1322ac5",
+    "url": "/static/media/showcase.72dfb9c8.jpg"
+  },
+  {
+    "revision": "6192e41a4b366ebc4eb86d4444c79177",
+    "url": "/static/media/spinner.6192e41a.gif"
+  }
+]);
